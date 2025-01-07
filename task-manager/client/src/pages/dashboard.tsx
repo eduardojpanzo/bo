2e5c74cbe7d6041db3b3f8bd7e2d75b7e164c9c6
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2025-01-07 05:25:55 +0100

    feat: componetes novos no dash

--- task-manager/client/src/pages/dashboard.tsx
+++ task-manager/client/src/pages/dashboard.tsx
@@ -1,7 +1,12 @@
+import { ResumeCalendar } from "@/components/resume-calendar";
+import { ResumeNumbers } from "@/components/resume-numbers";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
 export default function Dashboard() {
   return (
     <main className="flex flex-col gap-5 lg:flex-row">
-      <section>
+      <section className="flex flex-col gap-8">
         <div className="grid grid-cols-1 w-[620px] h-40 rounded-xl bg-accent sm:grid-cols-2 overflow-hidden">
           <div className="flex flex-col justify-center p-4">
             <strong className="text-3xl font-medium">Olá João!</strong>
@@ -15,9 +20,28 @@ export default function Dashboard() {
             />
           </figure>
         </div>
+
+        <div className="flex gap-5 items-center">
+          <ResumeNumbers desc="Horas traballhadas" title="11" />
+          <ResumeNumbers desc="Tarefas Criadas" title="30" />
+          <ResumeNumbers desc="Tarefas concluida" title="17" />
+        </div>
       </section>
 
-      <aside>.....</aside>
+      <aside className="max-w-96">
+        <div className="w-96 h-24 mx-auto flex items-center justify-between p-4 rounded-xl bg-accent">
+          <p>
+            <strong>Crie tarefa</strong> <br />
+            <span>crie uma nova tarefa</span>
+          </p>
+
+          <Button>
+            <Plus />
+          </Button>
+        </div>
+
+        <ResumeCalendar />
+      </aside>
     </main>
   );
 }
