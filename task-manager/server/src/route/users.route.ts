7f344a43b6a2f4e4c355d2c4861ac97da2c95f16
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2025-01-14 21:27:40 +0100

    chore: change a user

--- task-manager/server/src/route/users.route.ts
+++ task-manager/server/src/route/users.route.ts
@@ -41,7 +41,16 @@ export default async function userRoutes(app: FastifyInstance) {
       }
 
       const user = await prisma.user.create({
-        data: { email, password: hashedPassword, role: Role.OWNER },
+        data: {
+          email,
+          password: hashedPassword,
+          role: Role.OWNER,
+          name: email.split("@")[0],
+        },
+      });
+
+      await prisma.category.create({
+        data: { name: "all", userId: user.id },
       });
 
       reply.code(201).send({
@@ -49,7 +58,7 @@ export default async function userRoutes(app: FastifyInstance) {
         message: "Usuário criado com sucesso",
       });
     } catch (err) {
-      reply.code(400).send({ error: "Erro ao criar a conta" });
+      reply.code(400).send({ message: "Erro ao criar a conta" });
     }
   });
 
@@ -59,7 +68,7 @@ export default async function userRoutes(app: FastifyInstance) {
 
       const user = await prisma.user.findUnique({ where: { email } });
       if (!user || !(await bcrypt.compare(password, user.password))) {
-        reply.code(401).send({ error: "Senha ou e-mail está inválido" });
+        reply.code(401).send({ message: "Senha ou e-mail está inválido" });
         return;
       }
 
@@ -141,9 +150,7 @@ export default async function userRoutes(app: FastifyInstance) {
   app.delete("/profile", { preHandler: authenticate }, async (req, reply) => {
     try {
       const { user } = userCredentials.parse(req.query);
-      console.log(req.query);
 
-      await prisma.task.deleteMany({ where: { userId: Number(user.id) } });
       await prisma.user.delete({ where: { id: Number(user.id) } });
 
       reply.send({ message: "Usuário removido" });
