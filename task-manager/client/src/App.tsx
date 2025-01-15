aab3ff6dbcec2d6484ec1d3b403fb60a232cbb57
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2025-01-15 03:20:55 +0100

    feat: set usequery

--- task-manager/client/src/App.tsx
+++ task-manager/client/src/App.tsx
@@ -1,15 +1,19 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
 import { DialogContextProvider } from "./contexts/dialog-context";
 import { RootRoutes } from "./routes";
+import { queryClient } from "./lib/query";
 
 function App() {
   return (
-    <DialogContextProvider>
-      <>
-        <RootRoutes />
-        <Toaster />
-      </>
-    </DialogContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <DialogContextProvider>
+        <>
+          <RootRoutes />
+          <Toaster />
+        </>
+      </DialogContextProvider>
+    </QueryClientProvider>
   );
 }
 
